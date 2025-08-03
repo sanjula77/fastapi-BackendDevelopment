535028641264b4e6c3bdbe33743f9d2c1cc041ba
Gihan sanjula <sanjulagihan94@gmail.com>
2025-08-03 15:50:46 +0530

    implement pydentic schema

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class PostData(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: Optional[int] = None
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.post("/post")
+async def create_item(payload: PostData):
+    return {"item": payload}
